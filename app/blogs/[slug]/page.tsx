93b8f951aacb59ea2c37449b3d6c814455c1e251
Takahiro Aikou <ta-aikou@heart-musen.com>
2023-09-01 23:03:57 +0900

    remove getProps

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -4,59 +4,39 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Tags from '@/components/atoms/tags'
 
-type Post = {
-  slug: string
-  frontmatter: any
-}
+export default async function Page({ params: { slug } }: { params: { slug: string } }) {
 
-async function getProps(slug: string) {
-  try {
-    const fileName = fs.readFileSync(`public/posts/${slug}.md`, 'utf-8')
-    const { data: frontmatter, content } = matter(fileName)
-
-    return {
-      frontmatter,
-      content
-    }
-
-  } catch (error) {
-    console.error(error)
-
-    return {}
-  }
-}
-
-export default async function Page({ params }: { params: { slug: string } }) {
-  const post = await getProps(params.slug)
+  const fileName = fs.readFileSync(`public/posts/${slug}.md`, 'utf-8')
+  const { data: frontmatter, content } = matter(fileName)
 
   const markdownIt = require('markdown-it')
   const md = new markdownIt()
 
   return (
     <div className='prose'>
-      <h1 className='dark:invert'>{post.frontmatter?.title}</h1>
+      <h1 className='dark:invert'>{frontmatter?.title}</h1>
       <div className="flex flex-col">
         <Image
           width={ 650 }
           height={ 340 }
-          alt={ post.frontmatter?.title }
-          src={ `/${post.frontmatter?.socialImage}`}
+          alt={ frontmatter?.title }
+          src={ `/${frontmatter?.socialImage}`}
         />
-        <h3 className='italic pl-6'>--- {post.frontmatter?.preamble}</h3>
+        <h3 className='italic pl-6'>--- {frontmatter?.preamble}</h3>
         <div className='flex justify-end p-2'>
-          <Tags tags={post.frontmatter?.tags} />
+          <Tags tags={frontmatter?.tags} />
         </div>
-        <h3 className='text-right'>Created at {post.frontmatter?.createdAt.toDateString()}</h3>
+        <h3 className='text-right'>Created at {frontmatter?.createdAt.toDateString()}</h3>
       </div>
-      <div className='dark:invert' dangerouslySetInnerHTML={{ __html: md.render(post.content) }} />
+      <div className='dark:invert' dangerouslySetInnerHTML={{ __html: md.render(content) }} />
       <div className='flex justify-between'>
-        <h2 className='dark:invert pl-4'>{post.frontmatter?.conclusion}</h2>
+        <h2 className='dark:invert pl-4'>{frontmatter?.conclusion}</h2>
         <div className='pr-6'>
           <Image
             width={ 160 }
             height={ 160 }
-            alt={ post.frontmatter?.title }
-            src={ `/${post.frontmatter?.stampImage}`}
+            alt={ frontmatter?.title }
+            src={ `/${frontmatter?.stampImage}`}
           />
         </div>
       </div>
